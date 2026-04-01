457b0c7a48b4f3de500a1122c0a5a1916c2ebe88
Aditya Kumar <adityaful07@gmail.com>
2026-04-01 23:35:41 +0530

    Update HelloUC4.java

--- HelloUC4/HelloUC4.java
+++ HelloUC4/HelloUC4.java
@@ -5,12 +5,10 @@ public class HelloUC4 {
 
     public static void main(String[] args) {
 
-        // If no names are given
         if (args.length == 0) {
             System.out.println("Hello World");
         } 
         
-        // If names are provided
         else {
             for (int i = 0; i < args.length; i++) {
                 System.out.println("Hello " + args[i]);
